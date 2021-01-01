7eba1ffe534e1b1ef13c86998426ea630b79cc57
Md Shahazad Uddin <shanudjn@gmail.com>
2021-01-01 11:09:40 +0530

    func : added code to get all the html elements

---
+++ app.js
@@ -0,0 +1,8 @@
+var day = document.querySelector('.day');
+var month = document.querySelector('.month');
+var year = document.querySelector('.year');
+var luckyNumber = document.querySelector('.lucky-number');
+var checkButton = document.querySelector('.check-button')
+
+
+
