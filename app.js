224c5cca136f9c4c5771e258887ee137950a2519
Md Shahazad Uddin <shanudjn@gmail.com>
2021-01-01 15:10:20 +0530

    feat : added code to find the individual digits and show results

--- app.js
+++ app.js
@@ -14,6 +14,32 @@ function getData() {
 
     var luckyNumber = luckyNumberInput.value;
 
+    calculate(day, month, year, luckyNumber);
 
-    console.log(day)
+}
+
+function getSum(number) {
+    var sum = 0;
+    while (number != 0) {
+        let temp = number % 10;
+        number = Math.floor(number / 10);
+        sum += temp;
+    }
+    return sum;
+}
+
+function calculate(day, month, year, luckyNumber) {
+    var finalSum = 0;
+    var daySum = getSum(day);
+    var monthSum = getSum(month);
+    var yearSum = getSum(year);
+
+    finalSum = (daySum + monthSum + yearSum);
+
+    if (finalSum % luckyNumber == 0) {
+        console.log('Yay')
+    }
+    else {
+        console.log('nay')
+    }
 }
