f82fc44f46e485f656e80b4a3390a9e782025caa
Md Shahazad Uddin <shanudjn@gmail.com>
2021-01-01 14:29:52 +0530

    completed layout

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var day = document.querySelector('.day');
-var month = document.querySelector('.month');
-var year = document.querySelector('.year');
-var luckyNumber = document.querySelector('.lucky-number');
+var dayInput = document.querySelector('.day');
+var monthInput = document.querySelector('.month');
+var yearInput = document.querySelector('.year');
+var luckyNumberInput = document.querySelector('.lucky-number');
 var checkButton = document.querySelector('.check-button')
 
 
+checkButton.addEventListener('click', getData);
 
+function getData() {
+    var day = dayInput.value;
+    var month = monthInput.value;
+    var year = yearInput.value;
+
+    var luckyNumber = luckyNumberInput.value;
+
+
+    console.log(day)
+}
